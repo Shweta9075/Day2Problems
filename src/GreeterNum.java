debc94cef3a60b90521309bb7143399ea71db73d
shweta <shwetabirajdar5872gmail.com>
2022-09-13 17:14:08 +0530

    Added : Sum of N natural number using while loop

--- src/GreeterNum.java
+++ src/GreeterNum.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class GreeterNum {
+public class GreeterNum {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int c=27;
+        if(a>=b && a>=c){
+            System.out.println(a + "  is the greatest");
+        }else{
+            if(b>=c && b>= a){
+                System.out.println(b +  " is the greatest");
+            } else if (c>=a && c>=b) {
+                System.out.println(c +  " is the greatest");
+
+            }
+        }
+    }
 }
