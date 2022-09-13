d7b554610a3eb36ec68ab586de5030f9a00a3cfc
shweta <shwetabirajdar5872gmail.com>
2022-09-13 19:06:28 +0530

    Added:Sum Of natural no in given range

---
+++ src/com/bridgelabz/SumOfNaturalNumInGivenRange.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class SumOfNaturalNumInGivenRange {
+    public static void main(String[] args) {
+            int n = 5;
+            int sum = 0;
+
+            for (int i = 1; i <= 5; i++) {
+                sum = sum + i;
+            }
+            System.out.println("The sum of N natural number is : " + sum);
+        }
+    }
+
