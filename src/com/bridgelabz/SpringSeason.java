03bcf4b5f3ffe7aced9efa4cc64130431eb8a858
shweta <shwetabirajdar5872gmail.com>
2022-09-14 17:27:55 +0530

    Added: Take input from command-line and check condition

---
+++ src/com/bridgelabz/SpringSeason.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class SpringSeason {
+    public static void main(String[] args) {
+        // Take input from command line m for Month
+        int m = Integer.parseInt(args[0]);
+        // d for Days
+        int d = Integer.parseInt(args[1]);
+        boolean isSpring =  (m == 3 && d >= 20 && d <= 31)
+                || (m == 4 && d >=  1 && d <= 30)
+                || (m == 5 && d >=  1 && d <= 31)
+                || (m == 6 && d >=  1 && d <= 20);
+
+        System.out.println(isSpring);
+    }
+
+
+    }
+
